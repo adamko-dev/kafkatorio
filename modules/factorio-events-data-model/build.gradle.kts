a63edb1c61343848f3b805d20230369e9576d3f5
Adam <897017+asemy@users.noreply.github.com>
2021-10-25 23:35:18 +0200

    mucking about with the data-model

--- modules/factorio-events-data-model/build.gradle.kts
+++ modules/factorio-events-data-model/build.gradle.kts
@@ -1,46 +1,52 @@
 plugins {
-  id("dev.adamko.factoriowebmap.archetype.base")
-  kotlin("multiplatform")
+//  id("dev.adamko.factoriowebmap.archetype.base")
+//  kotlin("multiplatform")
+
+  id("dev.adamko.factoriowebmap.archetype.kotlin-jvm")
+
+  id("com.google.protobuf") version "0.8.17"
+
+
 }
 
 val projectId: String by project.extra
 val buildDir: Directory = layout.buildDirectory.dir(projectId).get()
 val nodeModulesDir: Directory by project.extra
 
-kotlin {
-
-  js(IR) {
-    binaries.executable()
-//    browser {
-//      commonWebpackConfig {
-//        cssSupport.enabled = true
+//kotlin {
+//
+//  js(IR) {
+//    binaries.executable()
+////    browser {
+////      commonWebpackConfig {
+////        cssSupport.enabled = true
+////      }
+////    }
+//
+//    nodejs()
+//    useCommonJs()
+//
+//    compilations["main"].apply {
+//      packageJson {
+//        customField(
+//          "scripts",
+//          mapOf(
+//            "build" to "tstl",
+//            "dev" to "tstl --watch",
+//          )
+//        )
 //      }
 //    }
-
-    nodejs()
-    useCommonJs()
-
-    compilations["main"].apply {
-      packageJson {
-        customField(
-          "scripts",
-          mapOf(
-            "build" to "tstl",
-            "dev" to "tstl --watch",
-          )
-        )
-      }
-    }
-
-  }
-
-  jvm {
-    compilations.all {
-      kotlinOptions.jvmTarget = "11"
-    }
-    testRuns["test"].executionTask.configure {
-      useJUnitPlatform()
-    }
-  }
-
-}
+//
+//  }
+//
+//  jvm {
+//    compilations.all {
+//      kotlinOptions.jvmTarget = "11"
+//    }
+//    testRuns["test"].executionTask.configure {
+//      useJUnitPlatform()
+//    }
+//  }
+//
+//}
