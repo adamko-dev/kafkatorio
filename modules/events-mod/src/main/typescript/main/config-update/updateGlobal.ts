042ca9fb98070e97bd8e78ddc9e2846a9dfcf085
Adam <897017+aSemy@users.noreply.github.com>
2022-01-08 23:10:04 +0100

    fix mod version and factorio version global def

--- modules/events-mod/src/main/typescript/main/config-update/updateGlobal.ts
+++ modules/events-mod/src/main/typescript/main/config-update/updateGlobal.ts
@@ -1,4 +1,4 @@
 export function updateGlobal() {
-  global.MOD_VERSION = script.active_mods[script.mod_name][1]
-  global.FACTORIO_VERSION = script.active_mods["base"][1]
+  global.MOD_VERSION = script.active_mods[script.mod_name]
+  global.FACTORIO_VERSION = script.active_mods["base"]
 }
