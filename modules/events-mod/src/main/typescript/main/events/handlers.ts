a8b13be110f3ce50ce148c8323f773df392b0564
Adam <897017+aSemy@users.noreply.github.com>
2022-01-09 19:07:51 +0100

    remove extra field (is there a better way to prevent this?)

--- modules/events-mod/src/main/typescript/main/events/handlers.ts
+++ modules/events-mod/src/main/typescript/main/events/handlers.ts
@@ -103,7 +103,6 @@ export function handleTilesUpdate(
         objectName: "LuaTile",
         position: it.position,
         prototypeName: newTileType.name,
-        surfaceIndex: surfaceIndex
       }
     })
 
