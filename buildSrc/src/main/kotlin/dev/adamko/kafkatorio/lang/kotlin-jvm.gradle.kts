8a57f9daea91af5853506fd6221d988161a692ad
Adam <897017+aSemy@users.noreply.github.com>
2022-02-26 22:24:42 +0100

    add commented out -Xextended-compiler-checks just in case it happens again

--- buildSrc/src/main/kotlin/dev/adamko/kafkatorio/lang/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/dev/adamko/kafkatorio/lang/kotlin-jvm.gradle.kts
@@ -47,6 +47,7 @@ tasks.withType<KotlinCompile>().configureEach {
     "-opt-in=kotlin.RequiresOptIn",
     "-opt-in=kotlin.ExperimentalStdlibApi",
     "-opt-in=kotlin.time.ExperimentalTime",
+//    "-Xextended-compiler-checks",
 //    "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
 //    "-opt-in=kotlinx.serialization.ExperimentalSerializationApi",
   )
