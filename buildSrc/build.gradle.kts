697a00b44391cfc4571c0e49f1639d178205e4e2
Adam <897017+aSemy@users.noreply.github.com>
2022-05-14 17:58:14 +0200

    bump kvision, kotlinx serialization, factorio, and a few others

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,50 +7,35 @@ plugins {
   `project-report`
 }
 
-
-object Versions {
-  const val jvmTarget = "11"
-  const val kotlinTarget = "1.6"
-
-  const val kotlin = "1.6.21"
-
-  const val gradleNodePlugin = "3.2.1"
-  const val gradleDockerComposePlugin = "0.15.2"
-
-  const val kotlinXSerialization = "1.3.2"
-  const val kvision = "5.8.2"
-  const val semver = "1.1.2"
-  const val http4k = "4.19.1.0"
-
-  const val kotest = "5.2.3"
-}
+val gradleJvmTarget = "11"
+val gradleKotlinTarget = "1.6"
 
 
 dependencies {
 
-  implementation(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom:${Versions.kotlin}"))
+  implementation(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom:${libs.versions.kotlin.get()}"))
   implementation("org.jetbrains.kotlin:kotlin-serialization")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
 
-  implementation(platform("org.jetbrains.kotlinx:kotlinx-serialization-bom:${Versions.kotlinXSerialization}"))
+  implementation(platform("org.jetbrains.kotlinx:kotlinx-serialization-bom:${libs.versions.kotlinx.serialization.get()}"))
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-core")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
 //  implementation("com.charleskorn.kaml:kaml:0.37.0")
 
-  implementation("com.github.node-gradle:gradle-node-plugin:${Versions.gradleNodePlugin}")
+  implementation("com.github.node-gradle:gradle-node-plugin:${libs.versions.gradleNodePlugin.get()}")
 
-  implementation("io.kotest:kotest-framework-multiplatform-plugin-gradle:${Versions.kotest}")
+  implementation("io.kotest:kotest-framework-multiplatform-plugin-gradle:${libs.versions.kotest.get()}")
 
-  implementation("io.kvision:io.kvision.gradle.plugin:${Versions.kvision}")
+  implementation("io.kvision:io.kvision.gradle.plugin:${libs.versions.kvision.get()}")
 
-  implementation("net.swiftzer.semver:semver:${Versions.semver}")
+  implementation("net.swiftzer.semver:semver:${libs.versions.semver.get()}")
 
   // https://github.com/avast/gradle-docker-compose-plugin
-  implementation("com.avast.gradle:gradle-docker-compose-plugin:${Versions.gradleDockerComposePlugin}")
+//  implementation("com.avast.gradle:gradle-docker-compose-plugin:${Versions.gradleDockerComposePlugin}")
 
-  implementation(platform("org.http4k:http4k-bom:${Versions.http4k}"))
+  implementation(platform("org.http4k:http4k-bom:${libs.versions.http4k.get()}"))
   implementation("org.http4k:http4k-core")
   implementation("org.http4k:http4k-client-okhttp")
 
@@ -68,12 +53,12 @@ dependencies {
 //  }
 //}
 
-tasks.withType<KotlinCompile>().all {
+tasks.withType<KotlinCompile>().configureEach {
 
   kotlinOptions {
-    jvmTarget = Versions.jvmTarget
-    apiVersion = Versions.kotlinTarget
-    languageVersion = Versions.kotlinTarget
+    jvmTarget = gradleJvmTarget
+    apiVersion = gradleKotlinTarget
+    languageVersion = gradleKotlinTarget
   }
 
   kotlinOptions.freeCompilerArgs += listOf(
@@ -88,11 +73,11 @@ tasks.withType<KotlinCompile>().all {
 
 kotlin {
   jvmToolchain {
-    (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(Versions.jvmTarget))
+    (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(gradleJvmTarget))
   }
 
   kotlinDslPluginOptions {
-    jvmTarget.set(Versions.jvmTarget)
+    jvmTarget.set(gradleJvmTarget)
   }
 }
 
@@ -101,11 +86,4 @@ idea {
     isDownloadSources = true
     isDownloadJavadoc = true
   }
-}
-
-// projects must be configuring hack
-
-
-tasks.configureEach {
-  inputs.property("pRoJeCtS-mUsT-bE-cOnFiGuRiNg", System.currentTimeMillis())
 }
