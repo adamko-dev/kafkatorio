dfc8b5c91df377a287bc86d14e355ebb71597ec1
Adam <897017+aSemy@users.noreply.github.com>
2022-06-24 15:10:40 +0200

    rm RUN from entrypoint.sh

--- modules/infra-kafka-cluster/src/entrypoint.sh
+++ modules/infra-kafka-cluster/src/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Format storage directories
-RUN ./bin/kafka-storage.sh format \
+./bin/kafka-storage.sh format \
   --ignore-formatted \
   --config ./server.properties \
   --cluster-id "$(cat cluster_id)"
