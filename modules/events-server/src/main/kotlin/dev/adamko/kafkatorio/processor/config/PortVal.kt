ae0d95b4cbd21d81e000638ea21ee4b531a39ce0
Adam <897017+aSemy@users.noreply.github.com>
2022-06-28 20:44:36 +0200

    remove old http4k application props

--- modules/events-server/src/main/kotlin/dev/adamko/kafkatorio/processor/config/PortVal.kt
+++ modules/events-server/src/main/kotlin/dev/adamko/kafkatorio/processor/config/PortVal.kt
@@ -1,13 +1,9 @@
 package dev.adamko.kafkatorio.processor.config
 
 import org.http4k.cloudnative.env.Port
-import org.http4k.lens.BiDiLensSpec
-import org.http4k.lens.StringBiDiMappings.int
-import org.http4k.lens.map
 
-/**
- * Improved (slightly) version of http4k's [Port].
- */
+
+/** Improved (slightly) version of http4k's [Port]. */
 @JvmInline
 value class PortVal(val value: Int) {
 
@@ -21,10 +17,6 @@ value class PortVal(val value: Int) {
 
   companion object {
     val DEFAULT = PortVal(8080)
-    val VALID_RANGE: IntRange
-      get() = (1..65535)
-
-    fun <IN : Any> BiDiLensSpec<IN, String>.portVal(): BiDiLensSpec<IN, PortVal> =
-      map(int().map(::PortVal, PortVal::value))
+    val VALID_RANGE: IntRange = 1..65535
   }
 }
