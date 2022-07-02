db4350a927b6852c32996ea9b3e0764f25b7bc02
Adam <897017+aSemy@users.noreply.github.com>
2022-07-02 08:54:38 +0200

    re-enable kafka processor

--- modules/events-server/src/main/kotlin/dev/adamko/kafkatorio/server/eventsServer.kt
+++ modules/events-server/src/main/kotlin/dev/adamko/kafkatorio/server/eventsServer.kt
@@ -1,10 +1,10 @@
 package dev.adamko.kafkatorio.server
 
 import dev.adamko.kafkatorio.server.config.ApplicationProperties
+import dev.adamko.kafkatorio.server.processor.eventsProcessor
 import dev.adamko.kafkatorio.server.socket.SyslogSocketServer
 import dev.adamko.kafkatorio.server.web.startWebServer
 import dev.adamko.kafkatorio.server.web.websocket.WebmapWebsocketServer
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
@@ -26,7 +26,7 @@ suspend fun main(): Unit = coroutineScope {
   }
 
 
-//  launch(Dispatchers.Default.limitedParallelism(2)) {
-//    eventsProcessor(appProps, syslogSocketServer, websocketServer)
-//  }
+  launch {
+    eventsProcessor(appProps, syslogSocketServer, websocketServer)
+  }
 }
